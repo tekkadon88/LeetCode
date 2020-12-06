055308b577404bfaaceedcd7e117cd8255dd2ada
tekkadon88 <74989380+tekkadon88@users.noreply.github.com>
2020-12-05 18:31:56 -0800

    Create 0001_twoSum.py

---
+++ 0001_twoSum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length = len(nums)
+        for i in range(length):
+            for j in range(i+1, length):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
